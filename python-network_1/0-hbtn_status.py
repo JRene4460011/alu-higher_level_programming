db0eee998b87c1f34657ed653b73ef2016438cf4
JRene4460011 <j.nsengiyum@alustudent.com>
2026-04-03 08:46:39 -0700

    asdf

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
 0-hbtn_status.py
-Fetches the status of https://alu-intranet.hbtn.io/status using urllib.
-Displays:
-    - type of response (bytes)
+Fetches a URL using urllib and displays the body response info:
+    - type
     - raw bytes content
     - utf-8 decoded content
 """
@@ -12,12 +11,15 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "http://0.0.0.0:5050/status"  # local server for testing
 
-    with urllib.request.urlopen(url) as response:
-        body = response.read()  # bytes
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()  # bytes
 
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except Exception as e:
+        print("Error:", e)
