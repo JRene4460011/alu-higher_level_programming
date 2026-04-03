ca10a4f925ed797d047d272ae3c7acb0c8d041af
JRene4460011 <j.nsengiyum@alustudent.com>
2026-04-03 05:55:57 -0700

    asdf

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -3,13 +3,24 @@
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Square class inheriting from Rectangle"""
 
     def __init__(self, size):
+        """Initialize a square with a given size
+
+        Args:
+            size (int): the size of the square (validated)
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
+    def __str__(self):
+        """Return the string representation of the square"""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
     def area(self):
+        """Return the area of the square"""
         return self.__size * self.__size
