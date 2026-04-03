86b99ec86dc3a4607bf31b4f9daacca034864ea7
JRene4460011 <j.nsengiyum@alustudent.com>
2026-04-03 05:44:28 -0700

    asdf

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Raise exception for area"""
-        raise Exception("area() is not implemented")`
+        raise Exception("area() is not implemented")
