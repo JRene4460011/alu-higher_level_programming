ffaf196d940933be3a872af6ba553e39a84cb5ae
JRene4460011 <j.nsengiyum@alustudent.com>
2026-04-03 08:50:13 -0700

    asdf

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """
 0-hbtn_status.py
-Fetches a URL using urllib and displays body response info:
+Fetches http://0.0.0.0:5050/status using urllib and displays body response info:
     - type
     - raw bytes content
     - utf-8 decoded content
 """
 
 import urllib.request
-import sys
 
 if __name__ == "__main__":
-    # Default to local server for container testing
     url = "http://0.0.0.0:5050/status"
-
-    # If URL provided as argument, use it
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
         body = response.read()  # bytes
