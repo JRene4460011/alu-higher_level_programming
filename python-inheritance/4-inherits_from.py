c1767d1f52f1db5d4663b2dec5f3ac540acf974d
JRene4460011 <j.nsengiyum@alustudent.com>
2026-04-03 06:08:17 -0700

    asdf

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,6 @@
 
 def inherits_from(obj, a_class):
     """Return True if obj is instance of subclass of a_class (not a_class itself)"""
-     return (isinstance(obj, a_class)
+
+    return (isinstance(obj, a_class)
         and type(obj) != a_class)
